0d6f046a2a77a2f1fe054951ff07a444ac7970ed
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-02-01 16:47:22 +0545

    fix : Added helper method for pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,13 +10,7 @@ class PagesController < ApplicationController
   def dashboard
     id = current_user.id
     role = current_user.role.identifier
-    case role
-    when 'user'
-      @tickets = Ticket.where(user_id: id)
-    when 'project_manager'
-      @projects = Project.where(project_manager_id: id)
-    when 'developer'
-      @tickets = Ticket.where(developer_id: id)
-    end
+    results = helpers.switch_dashboard(id, role)
+    results.include?(:tickets) ? @tickets = results : @projects = results
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  def switch_dashboard(id, role)
+    case role
+    when 'user'
+      @tickets = Ticket.where(user_id: id)
+    when 'project_manager'
+      @projects = Project.where(project_manager_id: id)
+    when 'developer'
+      @tickets = Ticket.where(developer_id: id)
+    end
+  end
 end
